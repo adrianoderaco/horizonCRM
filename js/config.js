d5b089d501704b590862ee2c509fccf50ce03001
Adriano Deraco <adrianoderaco@gmail.com>
2026-04-16 21:28:09 -0300

    Inverte index para cliente e adiciona agente.html

--- js/config.js
+++ js/config.js
@@ -1,4 +1,5 @@
+// Arquivo: js/config.js
 const CONFIG = {
-    URL: "https://seu-projeto.supabase.co",
-    KEY: "sua-anon-key-publica"
+    SUPABASE_URL: "https://corowdsyuirbxnepbhuh.supabase.co",
+    SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNvcm93ZHN5dWlyYnhuZXBiaHVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYzNzQ2NTQsImV4cCI6MjA5MTk1MDY1NH0.jLVf8l-QEPSzc_NpFIrrSvef-o54ki-5prnu--KZHrk"
 };
